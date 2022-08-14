a44548491fcf4d30b5f04e49b3f9cfca6b26e8a9
goflutterjava <goflutterjava@gmail.com>
2022-08-14 22:07:30 +0800

    use kgnet step 2

--- pkg/network/kafka.go
+++ pkg/network/kafka.go
@@ -42,29 +42,18 @@ var serverConfig *kgnet.GnetConfig
 func Run(config *kgnet.GnetConfig, kfkProtocolConfig *KafkaProtocolConfig, impl service.KfkServer) (*Server, error) {
 	serverConfig = config
 	server := &Server{
-		EventServer:         nil,
 		kafkaProtocolConfig: kfkProtocolConfig,
 		kafkaImpl:           impl,
 	}
-	encoderConfig := gnet.EncoderConfig{
-		ByteOrder:                       binary.BigEndian,
-		LengthFieldLength:               4,
-		LengthAdjustment:                0,
-		LengthIncludesLengthFieldLength: false,
+	k := &KafkaServer{
+		server,
 	}
-	decoderConfig := gnet.DecoderConfig{
-		ByteOrder:           binary.BigEndian,
-		LengthFieldOffset:   0,
-		LengthFieldLength:   4,
-		LengthAdjustment:    0,
-		InitialBytesToStrip: 4,
-	}
-	kfkCodec := gnet.NewLengthFieldBasedFrameCodec(encoderConfig, decoderConfig)
+	kafkaServer := kgnet.NewKafkaServer(*config, k)
 	go func() {
-		err := gnet.Serve(server, fmt.Sprintf("tcp://%s:%d", config.ListenHost, config.ListenPort), gnet.WithNumEventLoop(config.EventLoopNum), gnet.WithCodec(kfkCodec))
+		err := kafkaServer.Run()
 		logrus.Error("kafsar broker started error ", err)
 	}()
-	return server, nil
+	return k.Server, nil
 }
 
 func Close() (err error) {
@@ -75,17 +64,131 @@ func Close() (err error) {
 	return
 }
 
+type KafkaServer struct {
+	Server *Server
+}
+
+func (k KafkaServer) OnInitComplete(server gnet.Server) (action gnet.Action) {
+	logrus.Info("Kafka Server started")
+	return
+}
+
+func (k KafkaServer) OnOpened(c gnet.Conn) (out []byte, action gnet.Action) {
+	if atomic.LoadInt32(&connCount) > k.Server.kafkaProtocolConfig.MaxConn {
+		logrus.Error("connection reach max, refused to connect ", c.RemoteAddr())
+		return nil, gnet.Close
+	}
+	connCount := atomic.AddInt32(&connCount, 1)
+	k.Server.ConnMap.Store(c.RemoteAddr(), c)
+	logrus.Info("new connection connected ", connCount, " from ", c.RemoteAddr())
+	return
+}
+
+func (k KafkaServer) OnClosed(c gnet.Conn, err error) (action gnet.Action) {
+	logrus.Info("connection closed from ", c.RemoteAddr())
+	k.Server.kafkaImpl.Disconnect(c.RemoteAddr())
+	k.Server.ConnMap.Delete(c.RemoteAddr())
+	k.Server.SaslMap.Delete(c.RemoteAddr())
+	atomic.AddInt32(&connCount, -1)
+	return
+}
+
+func (k KafkaServer) InvalidKafkaPacket(c gnet.Conn) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (k KafkaServer) ConnError(c gnet.Conn, r any, stack []byte) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (k KafkaServer) UnSupportedApi(c gnet.Conn, apiKey codec.ApiCode, apiVersion int16) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (k KafkaServer) ApiVersion(c gnet.Conn, req *codec.ApiReq) (*codec.ApiResp, gnet.Action) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (k KafkaServer) Fetch(c gnet.Conn, req *codec.FetchReq) (*codec.FetchResp, gnet.Action) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (k KafkaServer) FindCoordinator(c gnet.Conn, req *codec.FindCoordinatorReq) (*codec.FindCoordinatorResp, gnet.Action) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (k KafkaServer) Heartbeat(c gnet.Conn, req *codec.HeartbeatReq) (*codec.HeartbeatResp, gnet.Action) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (k KafkaServer) JoinGroup(c gnet.Conn, req *codec.JoinGroupReq) (*codec.JoinGroupResp, gnet.Action) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (k KafkaServer) LeaveGroup(c gnet.Conn, req *codec.LeaveGroupReq) (*codec.LeaveGroupResp, gnet.Action) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (k KafkaServer) ListOffsets(c gnet.Conn, req *codec.ListOffsetsReq) (*codec.ListOffsetsResp, gnet.Action) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (k KafkaServer) Metadata(c gnet.Conn, req *codec.MetadataReq) (*codec.MetadataResp, gnet.Action) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (k KafkaServer) OffsetCommit(c gnet.Conn, req *codec.OffsetCommitReq) (*codec.OffsetCommitResp, gnet.Action) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (k KafkaServer) OffsetFetch(c gnet.Conn, req *codec.OffsetFetchReq) (*codec.OffsetFetchResp, gnet.Action) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (k KafkaServer) OffsetForLeaderEpoch(c gnet.Conn, req *codec.OffsetForLeaderEpochReq) (*codec.OffsetForLeaderEpochResp, gnet.Action) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (k KafkaServer) Produce(c gnet.Conn, req *codec.ProduceReq) (*codec.ProduceResp, gnet.Action) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (k KafkaServer) SaslAuthenticate(c gnet.Conn, req *codec.SaslAuthenticateReq) (*codec.SaslAuthenticateResp, gnet.Action) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (k KafkaServer) SaslHandshake(c gnet.Conn, req *codec.SaslHandshakeReq) (*codec.SaslHandshakeResp, gnet.Action) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (k KafkaServer) SyncGroup(c gnet.Conn, req *codec.SyncGroupReq) (*codec.SyncGroupResp, gnet.Action) {
+	//TODO implement me
+	panic("implement me")
+}
+
 type Server struct {
 	*gnet.EventServer
 	ConnMap             sync.Map
 	SaslMap             sync.Map
 	kafkaProtocolConfig *KafkaProtocolConfig
 	kafkaImpl           service.KfkServer
-}
-
-func (s *Server) OnInitComplete(server gnet.Server) (action gnet.Action) {
-	logrus.Info("Kafka Server started")
-	return
 }
 
 // React Kafka 协议格式为APIKey和API Version
@@ -181,24 +284,4 @@ func (s *Server) React(frame []byte, c gnet.Conn) (bytes []byte, g gnet.Action) 
 
 	logrus.Error("unknown api ", apiKey, apiVersion)
 	return nil, gnet.Close
-}
-
-func (s *Server) OnOpened(c gnet.Conn) (out []byte, action gnet.Action) {
-	if atomic.LoadInt32(&connCount) > s.kafkaProtocolConfig.MaxConn {
-		logrus.Error("connection reach max, refused to connect ", c.RemoteAddr())
-		return nil, gnet.Close
-	}
-	connCount := atomic.AddInt32(&connCount, 1)
-	s.ConnMap.Store(c.RemoteAddr(), c)
-	logrus.Info("new connection connected ", connCount, " from ", c.RemoteAddr())
-	return
-}
-
-func (s *Server) OnClosed(c gnet.Conn, err error) (action gnet.Action) {
-	logrus.Info("connection closed from ", c.RemoteAddr())
-	s.kafkaImpl.Disconnect(c.RemoteAddr())
-	s.ConnMap.Delete(c.RemoteAddr())
-	s.SaslMap.Delete(c.RemoteAddr())
-	atomic.AddInt32(&connCount, -1)
-	return
 }
